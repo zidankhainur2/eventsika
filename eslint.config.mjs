9de8b707cb0c7687695ad2e55efc3e8da7a75008
Ahmad Fauzidan <zidankhainur2@gmail.com>
2025-09-24 21:26:11 +0700

    fix: eslint error type any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
   },
 ];
 
